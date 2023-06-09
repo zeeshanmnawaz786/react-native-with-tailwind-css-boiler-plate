b8bd335366bab45543b4ef8eea21a6aacc002139
Zeeshan Nawaz <zeeshanm.nawaz786@gmail.com>
2023-06-09 23:48:42 +0500

    react native with tailwind css boiler-plate

---
+++ AwesomeProject/App.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text className='bg-black text-white p-2 text-center'>App</Text>
+    </View>
+  );
+};
+
+export default App;
